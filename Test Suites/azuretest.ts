6b812a37f55f8d63165b80e7bc78065b9a8e0490
Moriam Isah <Moriam.Isah@fcmb.com>
2021-11-01 18:13:22 +0100

    googletest

---
+++ Test Suites/azuretest.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>azuretest</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>fe2d0d0a-7033-48f9-b9e2-253c02a23594</testSuiteGuid>
+   <testCaseLink>
+      <guid>0dce2f56-d651-4b84-ae8d-47eab5aa9a5a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/azuretest</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
